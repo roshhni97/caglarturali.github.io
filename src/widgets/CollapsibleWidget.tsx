2e098c715972533fb95d2876d46c60f9c419bd1e
Çağlar Turalı <23704367+caglarturali@users.noreply.github.com>
2024-10-05 07:51:58 +0300

    Customize summary marker

--- src/widgets/CollapsibleWidget.tsx
+++ src/widgets/CollapsibleWidget.tsx
@@ -1,4 +1,6 @@
 import { PropsWithChildren } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 export type CollapsibleWidgetProps = PropsWithChildren<{
   title: string;
@@ -13,10 +15,14 @@ export default function CollapsibleWidget({
   return (
     <details
       open={open}
-      className="border-b border-dashed border-gray-500/50 pb-3"
+      className="group border-b border-dashed border-gray-500/50 pb-3"
     >
-      <summary className="cursor-pointer select-none font-sans-narrow text-sm font-light uppercase opacity-75">
-        {title}
+      <summary className="cursor-pointer select-none list-none font-sans-narrow text-sm font-light uppercase">
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className="opacity-50 transition-transform group-open:rotate-180"
+        />
+        <span className="ps-1 opacity-75 lg:ps-2">{title}</span>
       </summary>
       <div>{children}</div>
     </details>
