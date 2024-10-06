b32efa8e9a0b82b7683dc07b7e2612f565ea134c
Çağlar Turalı <23704367+caglarturali@users.noreply.github.com>
2024-10-06 09:56:48 +0300

    Prompt user for tts lang since they might not be living in their home country :grin:

--- scripts/processResume.ts
+++ scripts/processResume.ts
@@ -1,21 +1,25 @@
+import readline from 'readline/promises';
 import { writeFile } from 'fs/promises';
 import { Resume } from 'src/types/Resume';
 import resumeJson from 'data/resume.json';
-import codesJson from './codes.json';
 
 const resume = resumeJson as Resume;
-const codes = codesJson as { [key: string]: string };
 
-function getLangCode(countryCode: string) {
-  const langs = codes[countryCode.toUpperCase()];
-  return langs ? langs.split(',')[0] : 'en-us';
+async function prompt(msg: string, def?: string) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const txt = def ? ` [${def}]` : '';
+  const res = await rl.question(`${msg}${txt}: `);
+  rl.close();
+  return res || def || '';
 }
 
-async function getTTS(outputPath: string) {
-  const { basics } = resume;
-  const name = encodeURIComponent(basics.name);
-  const lang = getLangCode(basics.location.countryCode);
-  const url = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=${lang}&q=${name}`;
+async function getTTS(text: string, outputPath: string) {
+  text = encodeURIComponent(text);
+  const lang = await prompt('Lang code for tts', 'en-us');
+  const url = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=${lang}&q=${text}`;
   const res = await fetch(url);
   if (!res.ok) {
     console.error(`Failed to download file: ${res.statusText}`);
@@ -26,4 +30,4 @@ async function getTTS(outputPath: string) {
   await writeFile(outputPath, buffer);
 }
 
-await getTTS('data/tts.mp3');
+await getTTS(resume.basics.name, 'data/tts.mp3');
